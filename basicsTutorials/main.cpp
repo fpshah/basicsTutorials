4d5ed4f108e495530ec3afbc6884a19b376d2d81
Falgun Shah <falgunshah@Falguns-MacBook-Pro.local>
2014-04-01 00:12:54 -0700

    Initial Commit

---
+++ basicsTutorials/main.cpp
@@ -0,0 +1,18 @@
+//
+//  main.cpp
+//  basicsTutorials
+//
+//  Created by Falgun Shah on 4/1/14.
+//  Copyright (c) 2014 Ozymandias. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[])
+{
+
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
+
